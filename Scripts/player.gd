a00cb1bba085ed5a66d629f0a6582708d636b7b1
Joshua Hall <unstableplazma@gmail.com>
2024-01-21 20:06:27 +0800

    added stick

---
+++ Scripts/player.gd
@@ -0,0 +1,19 @@
+extends AnimatedSprite2D
+
+@export var speed = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
